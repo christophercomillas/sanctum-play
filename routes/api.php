c6c552aee8e936381f90332207ba7dd5febbbeb4
TSE <koyee>
2022-05-23 15:56:56 +0800

    spatie functionality

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Route::resource('products',ProductController::class);
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
 Route::get('/products', [ProductController::class,'index']);
 Route::get('/products/{id}', [ProductController::class,'show']);
-Route::get('/products/search/{name}', [ProductController::class,'search']);
 
 //Route::get('/products/search/{name}', [ProductController::class,'search']);
 // Route::get('/products', [ProductController::class,'index']);
@@ -29,6 +31,9 @@ Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/products', [ProductController::class,'store']);
     Route::put('/products/{id}', [ProductController::class,'update']);
     Route::delete('/products/{id}', [ProductController::class,'destroy']);
+    Route::get('/products/search/{name}', [ProductController::class,'search']);
+
+    Route::post('/logout', [AuthController::class,'logout']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
